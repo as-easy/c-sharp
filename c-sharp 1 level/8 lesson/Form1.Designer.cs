cfe2c768cc37d6de33028903bcb243e21b2d96f3
as-easy <varankinva@yandex.ru>
2017-11-27 16:42:32 +0300

    8.0 lesson

---
+++ c-sharp 1 level/8 lesson/Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace _8_lesson
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 36);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(280, 60);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(310, 36);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(103, 41);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Ввести вопрос";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(6, 20);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(53, 17);
+            this.checkBox1.TabIndex = 2;
+            this.checkBox1.Text = "Верю";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(6, 43);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(69, 17);
+            this.checkBox2.TabIndex = 2;
+            this.checkBox2.Text = "Не верю";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.checkBox1);
+            this.groupBox1.Controls.Add(this.checkBox2);
+            this.groupBox1.Location = new System.Drawing.Point(310, 83);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(103, 74);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(310, 174);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(103, 41);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Вывод вопросов";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 174);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(280, 60);
+            this.textBox2.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(470, 265);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ c-sharp 1 level/8 lesson/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _8_lesson
+{
+    public partial class Form1 : Form
+    {
+        List<string> questions = new List<string>();
+        
+        
+        public Form1()
+        {
+            InitializeComponent();
+            button1.Click += new EventHandler(this.mybutton1_click);
+            button2.Click += new EventHandler(this.mybutton2_click);
+            questions.Clear();
+
+            // Установите для свойства Multiline значение true. 
+            textBox1.Multiline = true;
+            // Добавьте вертикальные полосы прокрутки в элемент управления TextBox.
+            textBox1.ScrollBars = ScrollBars.Vertical;
+            // Разрешить RETURN в элементе управления TextBox. 
+            textBox1.AcceptsReturn = true;
+            // Разрешить ввод ключа TAB в элементе управления TextBox. 
+            textBox1.AcceptsTab = true;
+            // Установите WordWrap в значение true, чтобы текст мог переноситься на следующую строку. 
+            textBox1.WordWrap = true;
+
+            textBox2.Multiline = true;
+            textBox2.ScrollBars = ScrollBars.Vertical;
+            textBox2.AcceptsReturn = true;
+            textBox2.AcceptsTab = true;
+            textBox2.WordWrap = true;
+        }
+
+        private void mybutton1_click(object sender, EventArgs e)
+        {
+            if (textBox1.Text.Length > 6)
+            {
+                questions.Add(textBox1.Text);
+                textBox1.Clear();
+            }
+            else
+            {
+                MessageBox.Show("Введите вопрос минимум из семи знаков!");
+            }
+        }
+
+        private void mybutton2_click(object sender, EventArgs e)
+        {
+            textBox2.Clear();
+            questions.ForEach(delegate (String q)
+           {
+               textBox2.Text += q + Environment.NewLine;
+           });
+
+        }
+    }
+}
