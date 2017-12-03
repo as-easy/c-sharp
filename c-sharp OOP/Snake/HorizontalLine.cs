f0ea66ada57cf6acba6aee5c9684d45211df8e7d
as-easy <varankinva@yandex.ru>
2017-12-04 01:05:32 +0300

    Snake, OOP, Point

---
+++ c-sharp OOP/Snake/HorizontalLine.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    class HorizontalLine
+    {
+        public HorizontalLine(int left_x, int right_x, int heigth, int length, char sym)
+        {
+
+        }
+    }
+}
---
+++ c-sharp OOP/Snake/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Point> plist = new List<Point>();
+            Point p1 = new Point(20, 20, '-');
+            plist.Add(p1);
+
+            p1.Draw();
+        }
+    }
+}
